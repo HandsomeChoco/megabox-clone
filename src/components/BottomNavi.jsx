e8dd8cbefd1ac9958953fc138c77582826056f6f
Kangbeom Kim <kkb931115@gmail.com>
2021-02-14 00:04:08 +0900

    최적화 완료
    이제 screen 프로퍼티의 상태값이 바뀔 때, BottomItem 컴포넌트가 리렌더링 되지 않습니다.

--- src/components/BottomNavi.jsx
+++ src/components/BottomNavi.jsx
@@ -1,20 +1,22 @@
-import { AiFillHome, AiFillShopping } from "react-icons/ai";
-import { FaTicketAlt } from "react-icons/fa";
-import { GiPopcorn } from "react-icons/gi";
-import { BsFillPersonFill } from "react-icons/bs";
-import { useAppDispatchContext, useAppStateContext } from "../context/AppContext";
-import React, { useCallback, useMemo } from "react";
+import { AiFillHome, AiFillShopping } from 'react-icons/ai';
+import { FaTicketAlt } from 'react-icons/fa';
+import { GiPopcorn } from 'react-icons/gi';
+import { BsFillPersonFill } from 'react-icons/bs';
+import {
+  useAppDispatchContext,
+  useAppStateContext,
+} from '../context/AppContext';
+import React, { useCallback, useMemo } from 'react';
 
 const itemInfo = [
-  { icon: <AiFillHome />, text: "홈", current: "home" },
-  { icon: <AiFillShopping />, text: "스토어", current: "store" },
-  { icon: <FaTicketAlt />, text: "예매", current: "booking" },
-  { icon: <GiPopcorn />, text: "모바일오더", current: "mobileOrder" },
-  { icon: <BsFillPersonFill />, text: "MY", current: "my" },
-]
+  { icon: <AiFillHome />, text: '홈', current: 'home' },
+  { icon: <AiFillShopping />, text: '스토어', current: 'store' },
+  { icon: <FaTicketAlt />, text: '예매', current: 'booking' },
+  { icon: <GiPopcorn />, text: '모바일오더', current: 'mobileOrder' },
+  { icon: <BsFillPersonFill />, text: 'MY', current: 'my' },
+];
 
 const BottomItem = React.memo(({ text, icon, current }) => {
-  const state = useAppStateContext();
   const dispatch = useAppDispatchContext();
 
   console.log('bottom item');
@@ -22,7 +24,7 @@ const BottomItem = React.memo(({ text, icon, current }) => {
   const setScreen = useCallback((e) => {
     dispatch({
       type: 'SET_SCREEN',
-      screen: e.currentTarget.name
+      screen: e.currentTarget.name,
     });
     // eslint-disable-next-line
   }, []);
@@ -38,8 +40,7 @@ const BottomItem = React.memo(({ text, icon, current }) => {
 });
 
 const BottomNavi = () => {
-  console.log('bottom navi')
-  
+  console.log('bottom navi');
   const memoItemInfo = useMemo(() => itemInfo, []);
 
   return (
