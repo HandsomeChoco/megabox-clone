5086cf49909d63b024416da1dccaf1ed204afcc9
Kangbeom Kim <kkb931115@gmail.com>
2021-02-14 01:59:54 +0900

    사이드바 보이기/숨기기 상태를 토글하는 액션 타입 추가

--- src/hooks/reducer/reducer.js
+++ src/hooks/reducer/reducer.js
@@ -1,13 +1,18 @@
 const reducer = (state, action) => {
-    switch(action.type) {
-      case 'SET_SCREEN' : return {
-          ...state, screen: action.screen
-      }
-      
-      default : throw new Error(`Unhandled action type: ${action.type}`)
-    }
-}
-
+  switch (action.type) {
+    case 'SET_SCREEN':
+      return {
+        ...state,
+        screen: action.screen,
+      };
+    case 'TOGGLE_SIDEBAR':
+      return {
+        ...state,
+        isSideBarHidden: !state.isSideBarHidden,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
 
 export default reducer;
-  
