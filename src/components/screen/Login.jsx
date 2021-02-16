6ddcaef36bb09f0f0679d6c9c815403ae8e8a590
Kangbeom Kim <kkb931115@gmail.com>
2021-02-17 00:11:01 +0900

    Login => IoMdClose, Login 컴포넌트의 상태를 토글할 수 있는 핸들러 할당

--- src/components/screen/Login.jsx
+++ src/components/screen/Login.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { IoMdClose } from 'react-icons/io';
+import {
+  useAppDispatchContext,
+  useAppStateContext,
+} from '../../context/AppContext';
 
 function Login() {
-    return(
-        <div className="login">
-            <div className='login-top'>
-                <div className='login-top-title'>
-                    <h3>
-                        로그인
-                    </h3>
-                </div>
-                <div className='login-top-icon'>
-                    <IoMdClose style={{ fontSize: 25 }} onClick={() => alert('test')}/>
-                </div>
-            </div>
+  console.log('login');
+  const state = useAppStateContext();
+  const dispatch = useAppDispatchContext();
+  const { isLoginWindowHidden } = state;
+
+  const toggleLoginWindow = useCallback(() => {
+    dispatch({
+      type: 'TOGGLE_LOGIN_WINDOW',
+    });
+  });
+
+  return (
+    <div className={isLoginWindowHidden ? 'loginHidden' : 'login'}>
+      <div className="login-top">
+        <div className="login-top-title">
+          <h3>로그인</h3>
         </div>
-    );
-};
+        <div className="login-top-icon">
+          <IoMdClose style={{ fontSize: 25 }} onClick={toggleLoginWindow} />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default React.memo(Login);
