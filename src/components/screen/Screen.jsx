e8a4d29f89fa19e9622bd383f4a032af8e5d7319
Kangbeom <kangbeom.kim@slexn.com>
2021-02-19 15:18:19 +0900

    최적화 결과보기 전 커밋

--- src/components/screen/Screen.jsx
+++ src/components/screen/Screen.jsx
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, { Suspense, useCallback } from 'react';
 import { Route } from 'react-router-dom';
 
-import Home from './Home';
-import Store from './Store';
-import Booking from './Booking';
-import MobileOrder from './MobileOrder';
-import My from './My';
+const Home = React.lazy(() => import('./Home'));
+const Store = React.lazy(() => import('./Store'));
+const Booking = React.lazy(() => import('./Booking'));
+const MobileOrder = React.lazy(() => import('./MobileOrder'));
+const My = React.lazy(() => import('./My'));
+const Login = React.lazy(() => import('./Login'));
 
 const Screen = () => {
   return (
     <>
-      <Route path="/" exact={true}>
-        <Home />
-      </Route>
-      <Route path="/store" exact={true}>
-        <Store />
-      </Route>
-      <Route path="/booking" exact={true}>
-        <Booking />
-      </Route>
-      <Route path="/mobile_order" exact={true}>
-        <MobileOrder />
-      </Route>
-      <Route path="/my" exact={true}>
-        <My />
-      </Route>
+      <Suspense fallback={'로딩 중...'}>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/store" exact={true} component={Store} />
+        <Route path="/booking" exact={true} component={Booking} />
+        <Route path="/mobile_order" exact={true} component={MobileOrder} />
+        <Route path="/my" exact={true} component={My} />
+        <Route path="/login" exact={true} component={Login} />
+      </Suspense>
     </>
   );
 };
