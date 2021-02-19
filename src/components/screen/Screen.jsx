8c9ac383a8bb0a186a51d8d702d62f787d283d5c
Kangbeom <kangbeom.kim@slexn.com>
2021-02-19 17:10:06 +0900

    App 컴포넌트 Screen 함수로 렌더링하던것 =>  라우팅으로 변경

--- src/components/screen/Screen.jsx
+++ src/components/screen/Screen.jsx
@@ -8,16 +8,37 @@ const MobileOrder = React.lazy(() => import('./MobileOrder'));
 const My = React.lazy(() => import('./My'));
 const Login = React.lazy(() => import('./Login'));
 
+/* <Route component={Component}/> 로 작성하면
+  같은 버튼을 눌렀을 때 리렌더링이 일어남. 이렇게 작성하니 그렇지 않음.
+*/
+
 const Screen = () => {
   return (
     <>
       <Suspense fallback={'로딩 중...'}>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/store" exact={true} component={Store} />
-        <Route path="/booking" exact={true} component={Booking} />
-        <Route path="/mobile_order" exact={true} component={MobileOrder} />
-        <Route path="/my" exact={true} component={My} />
-        <Route path="/login" exact={true} component={Login} />
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+
+        <Route path="/store" exact={true}>
+          <Store />
+        </Route>
+
+        <Route path="/booking" exact={true}>
+          <Booking />
+        </Route>
+
+        <Route path="/mobile_order" exact={true}>
+          <MobileOrder />
+        </Route>
+
+        <Route path="/my" exact={true}>
+          <My />
+        </Route>
+
+        <Route path="/login" exact={true}>
+          <Login />
+        </Route>
       </Suspense>
     </>
   );
