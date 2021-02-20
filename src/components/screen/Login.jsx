f8813a4fa15ffc76ad7485266d72201d3b9335b4
HandsomeChoco <kkb931115@gmail.com>
2021-02-21 01:36:39 +0900

    로그인 페이지 화면 설계

--- src/components/screen/Login.jsx
+++ src/components/screen/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
+import { RiKakaoTalkFill } from 'react-icons/ri'
 import { IoMdClose } from 'react-icons/io';
-import { Link } from 'react-router-dom'
+import { FaFacebook, FaApple } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
 import {
   useAppDispatchContext,
   useAppStateContext,
@@ -22,16 +25,16 @@ function Login({ history }) {
     toggleLoginWindow();
     // history.goBack();
   };
-  
+
   useEffect(() => {
-    if (document.URL==='http://localhost:3000/login' && isLoginWindowHidden) {
+    if (document.URL === 'http://localhost:3000/login' && isLoginWindowHidden) {
       toggleLoginWindow();
     }
+    //eslint-disable-next-line
   }, []);
 
   return (
     <div className={isLoginWindowHidden ? 'login loginHidden' : 'login'}>
-      
       <div className="login-top">
         <div className="login-top-title">
           <h3>로그인</h3>
@@ -41,36 +44,43 @@ function Login({ history }) {
         </div>
       </div>
 
-      <div className='login-middle-input'>
-        <input type="text" placeholder='아이디' />
-        <input type="password" placeholder='비밀번호' />
+      <div className="login-middle-input">
+        <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
       </div>
 
       <div className="login-middle-checkbox">
-        <input type="checkbox" name="" id="auto-login"/> 
-        <label for='auto-login'>자동 로그인</label>
+        <input type="checkbox" name="" id="auto-login" />
+        <label for="auto-login">자동 로그인</label>
       </div>
 
-      <div className='login-middle-form'>
-        <form action="">
-          <button className='login-button'>로그인</button>
+      <div className="login-middle-form">
+        <form action="" method="POST">
+          <button className="login-button">로그인</button>
         </form>
       </div>
 
-      <div className='login-bottom-links'>
-        <div>
-          <Link><span>ID/PW 찾기</span></Link> |
-          <Link><span>회원가입</span></Link> |&nbsp;
-          <Link><span>비회원 예매확인</span></Link>
+      <div className="login-bottom-links">
+        <div className="login-bottom-account">
+          <Link to="/">
+            <span>ID/PW 찾기</span>
+          </Link>
+          |
+          <Link to="/">
+            <span>회원가입</span>
+          </Link>
+          |
+          <Link to="/">
+            <span>비회원 예매확인</span>
+          </Link>
         </div>
-        <div>
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
+        <div className="login-bottom-social">
+          <span><FaFacebook className='fb'/></span>
+          <span><FcGoogle className='google'/></span>
+          <span><RiKakaoTalkFill className='kakao'/></span>
+          <span><FaApple className='apple'/></span>
         </div>
       </div>
-      
     </div>
   );
 }
