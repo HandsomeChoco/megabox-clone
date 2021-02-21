932f1afddb4d24719071b5c2c9f10171b989368e
Kangbeom Kim <kkb931115@gmail.com>
2021-02-21 20:28:27 +0900

    재사용 가능한 컴포넌트 단위 만들기

--- src/components/reusable/Form.jsx
+++ src/components/reusable/Form.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Form = ({ children }) => {
-  const [state, setState] = useState({});
-  console.log(children[0]);
+  const [state, setState] = useState();
+  console.log(children.map(v => v.props));
   return <form>{children}</form>;
 };
 
