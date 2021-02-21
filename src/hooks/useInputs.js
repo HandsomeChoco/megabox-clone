484f6aa711ceb2cf3c4d3f0d4a4ba2202e914ac1
HandsomeChoco <kkb931115@gmail.com>
2021-02-22 01:34:12 +0900

    입력 상태관리하는 커스텀 훅 제작

---
+++ src/hooks/useInputs.js
@@ -0,0 +1,14 @@
+import React, { useCallback, useState } from "react"
+
+const useInputs = (initState) => {
+    const [inputState, setInputState] = useState(initState);
+    
+    const onChange = useCallback(e => {
+        const { name, value } = e.target;
+        setInputState({ ...inputState, [name]: value })
+    }, []);
+
+    return  [inputState, onChange];
+}
+
+export default useInputs;
