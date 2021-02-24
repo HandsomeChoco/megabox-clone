b8c8e7f34abc0c3431af6c1e4ca3a7ad1284b682
Kangbeom <kangbeom.kim@slexn.com>
2021-02-24 15:04:33 +0900

    Home 컴포넌트에 HashTags 컴포넌트를 구현했습니다.
    더불어 Home 컴포넌트와 관련된 세세한 스타일이 일부 수정되었습니다.

--- src/components/screen/Home.jsx
+++ src/components/screen/Home.jsx
@@ -15,8 +15,28 @@ const Story = React.memo(() => {
   );
 });
 
+const tags = [
+  '박스오피스',
+  '상영예정',
+  '단독',
+  'N스크린',
+  '돌비시네마',
+  '클소',
+  '필소',
+];
+
+const HashTag = React.memo(({ text }) => {
+  return <span className="hash-tag">{text}</span>;
+});
+
 const Rank = React.memo(() => {
-  return <div className="home-body-rank">MovieRank</div>;
+  return (
+    <div className="home-body-rank">
+      {tags.map((tag, index) => (
+        <HashTag text={'#' + tag} key={index} />
+      ))}
+    </div>
+  );
 });
 
 const MovieFeed = React.memo(() => {
