2e7a140df65e9f7ce93c2516ef8ef10b5b22d76a
Kangbeom Kim <kkb931115@gmail.com>
2021-02-27 22:44:32 +0900

    미리 스타일이 정의된 div 태그를 사용해 Store 컴포넌트에 테스트 중입니다.

--- src/components/routes/store/Store.jsx
+++ src/components/routes/store/Store.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import Div from '../../reusable/Div';
 // this component is not a Redux store!
 
 const Store = () => {
   console.log('Store');
-  return <div style={{ color: 'white' }}>스토어</div>;
+  return (
+    <div style={{ color: 'white' }}>
+      스토어
+      <Div
+        className="test"
+        flex={true}
+        flow="column"
+        justifyContent="center"
+        alignItems="flexEnd"
+      >
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </Div>
+    </div>
+  );
 };
 
 export default React.memo(Store);
