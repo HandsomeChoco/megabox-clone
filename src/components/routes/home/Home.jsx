a455f34b5823fdd888f6fa8cbd5e12b2f184fed4
Kangbeom <kangbeom.kim@slexn.com>
2021-03-03 18:04:45 +0900

    Rank 컴포넌트가 반응형으로 변경되었습니다.

--- src/components/routes/home/Home.jsx
+++ src/components/routes/home/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Axios from 'axios';
+
 import Story from './Story';
 import Rank from './Rank';
 import MovieFeed from './MovieFeed';
@@ -6,6 +8,16 @@ import GreetAndPreScreen from './GreetAndPreScreen';
 import Event from './Event';
 import Special from './Special';
 import MoviePost from './MoviePost';
+
+import useNetwork from '../../../hooks/useNetwork';
+
+import getYesterDay from '../../../lib/date';
+import key from '../../../lib/key.json';
+
+// const getDailyBoxOfficeAPI = async () => {
+//   .then(response => response.data);
+//   return result;
+// };
 
 const HomeBody = React.memo(() => {
   return (
@@ -21,7 +33,23 @@ const HomeBody = React.memo(() => {
 });
 
 const Home = () => {
+  const [state, setState] = useNetwork();
   console.log('Home');
+
+  const result = async () => {
+    let result;
+    try {
+      //prettier-ignore
+      result = await Axios.get(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key.privateKey}&targetDt=${getYesterDay()}`);
+    } catch (e) {
+      console.error(e);
+    }
+    return result.data;
+  };
+  result();
+
+  useEffect(() => {});
+
   return (
     <div className="home">
       <Story />
